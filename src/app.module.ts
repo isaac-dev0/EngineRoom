1c09cd2a774bffa7acb5a425c5a09696d9ef3952
Isaac Dolphin <isaac_dolphin@outlook.com>
2025-06-14 20:21:33 +0100

    Changes to file path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './core/database/database.module';
 import { ReferralModule } from './referral/referral.module';
 import { ConfigModule } from './core/config/config.module';
-import { CommentModule } from './comments/comment.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [ConfigModule, DatabaseModule, ReferralModule, CommentModule],
